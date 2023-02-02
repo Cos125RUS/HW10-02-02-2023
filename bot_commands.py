ef787e35954c8446d7642be5cd4305e93c3d288f
Valerii_Panov <val250640@mail.ru>
2023-02-02 11:21:50 +1000

    addButtons

--- bot_commands.py
+++ bot_commands.py
@@ -1,8 +1,16 @@
+# Модули telegram
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
+
+# Модуль логирования
 from spy import *
+
+# Модуль работы с данными
 from weather import *
 
+
+#-------------------------------------------------------------------------------------
+# Привтствие юзера
 async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     update.message.chat.id # id пользователя
     update.effective_user.first_name # first_name пользователя на входе
@@ -12,43 +20,54 @@ async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # user_id = update.effective_user.id 
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+#-------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------
+# Подсказка для пользователей
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     await update.message.reply_text(f'/weather <<City>>\nFor example:\n/weather Moscow')
 
+#-------------------------------------------------------------------------------------
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#-------------------------------------------------------------------------------------
+# Отрисовка кнопок
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
-    alpha = "АБВГДЕЖЗИК"
-    keyboard = [[InlineKeyboardButton("Владивосток", callback_data="Владивосток"), 
-        InlineKeyboardButton("Томск", callback_data="Томск")],  
-        [InlineKeyboardButton("Хабаровск", callback_data="Хабаровск "), 
-        InlineKeyboardButton("Новосибирск", callback_data="Новосибирск")],
-        [InlineKeyboardButton("Краснодар", callback_data="Краснодар"), 
-        InlineKeyboardButton("Екатеринбург", callback_data="Екатеринбург")], 
-        [InlineKeyboardButton("Москва", callback_data="Москва"), 
-        InlineKeyboardButton("Санкт-Петербург", callback_data="Санкт-Петербург")]]
+    keyboard = [[InlineKeyboardButton("Томск", callback_data="Tomsk"),
+        InlineKeyboardButton("Владивосток", callback_data="Vladivostok"), 
+        InlineKeyboardButton("Новокузнецк", callback_data="Novokuznetsk")],  
+        [InlineKeyboardButton("Гродно", callback_data="Grodno"), 
+        InlineKeyboardButton("Вунгтау", callback_data="Vung Tau"),
+        InlineKeyboardButton("Шымкент", callback_data="Shymkent")],
+        [InlineKeyboardButton("Мурманск", callback_data="Красногорск"), 
+        InlineKeyboardButton("Уфа", callback_data="Ставрополь"),
+        InlineKeyboardButton("Барнаул", callback_data="Barnaul")], 
+        [InlineKeyboardButton("Москва", callback_data="Moscow"), 
+        InlineKeyboardButton("Санкт-Петербург", callback_data="St. Petersburg"),
+        InlineKeyboardButton("Казань", callback_data="Kazan")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Выберите город:", reply_markup=reply_markup)
 
+#-------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------
+# Считывание нажатий
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
-    print(query.data) # Нажатая кнопка
-    await query.answer(query.data)
-    await query.edit_message_text(text=f"{query.data}: weather") # Ответ пользователю
+    answer = get_answer(query.data)
+    await query.answer()
+    await query.edit_message_text(text=answer) # Ответ пользователю
 
+#-------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------
+# Ответ на команду /weather
 async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
-    # print(update.message.text.replace('/weather ',''))
     cityName = update.message.text.replace('/weather ','')
-    weather = get_weather_by_city(f"{cityName}")
-    temp = weather['main']['temp']
-    windSpeed = weather['wind']['speed']
-    cloudiness = weather['weather'][0]['main']
-    answer = f'Temp = {temp}\nWind speed = {windSpeed}\nCloudiness = {cloudiness}'
-    await update.message.reply_text(answer)
+    await update.message.reply_text(get_answer(cityName))
 
+#-------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------
